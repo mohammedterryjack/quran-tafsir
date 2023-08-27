99a0bba66194e8c6ca97a7a764ff89db66954c4e
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2023-08-27 10:19:20 +0100

    Update create_english_arabic_dictionary.py

--- arabic/dictionary/create_english_arabic_dictionary.py
+++ arabic/dictionary/create_english_arabic_dictionary.py
@@ -9,7 +9,7 @@ def remove_diacritics(arabic_text:str) -> str:
     return ''.join([c for c in normalized_text if not combining(c)]).replace("أ", "ا").replace('ٱ',"ا").replace("ـ", "").replace('_',' ')
 
 freq_dist = FreqDist(brown.words())
-rarity_threshold = 100  
+rarity_threshold = 50  
 
 results = {}
 for word in wordnet.words():
@@ -20,20 +20,28 @@ for word in wordnet.words():
         word_frequency = freq_dist[word]
         if word_frequency<rarity_threshold:
             continue
+        pos = synset.pos()
         print(word)
         print(arabic_synset)
-        for lemma in list(map(lambda word:word.replace('-',' ').replace('_',' '), synset.lemma_names())):            
+        for lemma in list(map(lambda word:word.replace('-',' ').replace('_',' '), synset.lemma_names())):
             lemma_frequency = freq_dist[lemma]
             if lemma_frequency<rarity_threshold:
                 continue
+            lemma = lemma.lower()
             for arabic_word in arabic_synset:
                 arabic_word = remove_diacritics(arabic_word)
-                if arabic_word.isdigit():
+                if arabic_word.isdigit() or len(arabic_word.split())>1:
                     continue
                 if lemma not in results:
-                    results[lemma] = set()
-                results[lemma].add(arabic_word)
-    
-    
+                    results[lemma] = {}
+                if pos not in results[lemma]:
+                    results[lemma][pos] = set()
+                results[lemma][pos].add(arabic_word)
+for word,arabic in results.items():
+    result = []
+    for pos,values in arabic.items():
+        result.append(f"{word} ({pos}): {'، '.join(sorted(values))}")
+    results[word] = '\n\n'.join(result)
+
 with open('dictionary.json','w') as f:
-    f.write(dumps(results,indent=2,ensure_ascii=False,default='، '.join))
+    f.write(dumps(results,indent=2,ensure_ascii=False))
